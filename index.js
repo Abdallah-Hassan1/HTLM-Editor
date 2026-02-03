236fc550aa64f8443d2e83100faaeb98a2150b83
Abdallah Hassan <91224794+Abdallah-Hassan1@users.noreply.github.com>
2026-02-03 02:43:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+//Function for live Rendering
+function update(i) {
+  let htmlCode = document.getElementById("htmlCode").value;
+  let cssCode = document.getElementById("cssCode").value;
+  let javascriptCode = document.getElementById("javascriptCode").value;
+  let text =
+    htmlCode +
+    "<style>" +
+    cssCode +
+    "</style>" +
+    "<scri" +
+    "pt>" +
+    javascriptCode +
+    "</scri" +
+    "pt>";
+  let iframe = document.getElementById("viewer").contentWindow.document;
+  iframe.open();
+  iframe.write(text);
+  iframe.close();
+}
