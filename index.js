22263f48a9e7b9ab18fad30e6b7f7036e34d39c7
Abdallah Hassan <91224794+Abdallah-Hassan1@users.noreply.github.com>
2026-02-05 21:50:02 +0200

    update console

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+// Function for show output of the console function
+function consoleViewer() {
+  let htmlCode = `<mainHTML>${document.getElementById("htmlCode").value}</mainHTML>
+  <console-output></console-output>`;
+
+  let cssCode = "console-output { width: 100%; } mainHTML { display: none; }";
+
+  let text = `${htmlCode}
+  <style>${cssCode}</style>
+  <script>
+  console.log = function (...args) {
+    //originalConsoleLog.apply(console, args);
+    const message = document.createElement("div");
+
+    const ouputMessage = args
+      .map((arg) => {
+        if (typeof arg === "object" && arg != null) return JSON.stringify(arg);
+        return String(arg);
+      })
+      .join(" ");
+
+    message.textContent = ouputMessage;
+    document.querySelector("console-output").appendChild(message);
+  };
+  ${document.getElementById("javascriptCode").value}
+  </script>`;
+
+  let consoleIframe = document.querySelector("#console-viewer");
+  consoleIframe.remove();
+  consoleIframe = document.createElement("iframe");
+  consoleIframe.setAttribute("id", "console-viewer");
+  document
+    .querySelector(".console-iframe-container")
+    .appendChild(consoleIframe);
+
+  consoleIframe.contentWindow.document.open();
+  consoleIframe.contentWindow.document.write(text);
+  consoleIframe.contentWindow.document.close();
+}
+
 //Function for live Rendering
 function update() {
   let htmlCode = document.getElementById("htmlCode").value;
@@ -21,48 +61,4 @@ function update() {
   iframe.contentWindow.document.write(text);
   iframe.contentWindow.document.close();
   consoleViewer();
-}
-
-// Function for show output of the console function
-function consoleViewer() {
-  let htmlCode = '<div class="console-output"></div>';
-
-  let cssCode = "div { width: 100%; }";
-
-  let text = `${htmlCode}
-  <style>${cssCode}</style>
-  <script>
-  // Show console ouput in the page
-  // let outputDiv = document.querySelector(".console-output");
-  // const originalConsoleLog = console.log;
-
-  console.log = function (...args) {
-    // originalConsoleLog.apply(console, args);
-
-    const message = document.createElement("div");
-
-    const ouputMessage = args
-      .map((arg) => {
-        if (typeof arg === "object" && arg != null) return JSON.stringify(arg);
-        return String(arg);
-      })
-      .join(" ");
-
-    message.textContent = ouputMessage;
-    document.querySelector(".console-output").appendChild(message);
-  };
-  ${document.getElementById("javascriptCode").value}
-  </script>`;
-
-  let consoleIframe = document.querySelector("#console-viewer");
-  consoleIframe.remove();
-  consoleIframe = document.createElement("iframe");
-  consoleIframe.setAttribute("id", "console-viewer");
-  document
-    .querySelector(".console-iframe-container")
-    .appendChild(consoleIframe);
-
-  consoleIframe.contentWindow.document.open();
-  consoleIframe.contentWindow.document.write(text);
-  consoleIframe.contentWindow.document.close();
 }
